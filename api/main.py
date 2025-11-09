3f679686c2721599a5659ce22592e03d9be3897a
Om Barvekar <jayaschool1981@gmail.com>
2025-11-09 17:27:39 +0530

    fix: CORS position

--- api/main.py
+++ api/main.py
@@ -11,14 +11,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ai-energy-forecast-frontend-eqsa7wsxx.vercel.app"
+        "https://ai-energy-forecast-frontend-j88m1dvxe.vercel.app",
+        "https://ai-energy-forecast-frontend-r7gakq0qd.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "models", "energy_mlp.pkl")
 model = joblib.load(MODEL_PATH)
@@ -27,11 +26,9 @@ class PredictIn(BaseModel):
     timestamp: str = Field(..., description="ISO timestamp e.g. 2025-11-08T14:00:00Z")
     temp_c: float = Field(..., description="Temperature in Celsius")
 
-
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
 
 @app.post("/predict")
 def predict(inp: PredictIn):
